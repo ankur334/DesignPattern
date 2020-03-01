f135dcc4816252d86adc71def7bd7dc982946f31
ankur <ankurranjan334@outlook.com>
2020-03-01 23:09:46 +0530

    Add: Basic Factory method for Song Serializer

--- CreationalPattern/Factory/song/serializers.py
+++ CreationalPattern/Factory/song/serializers.py
@@ -3,22 +3,33 @@ import xml.etree.ElementTree as et
 
 
 class Serializer:
-    def serializer(self, song, _format):
+
+    @staticmethod
+    def _serialization_to_json(song):
+        song_info = {
+            'id': song.id,
+            'title': song.title,
+            'artist': song.artist
+        }
+        return json.dumps(song_info)
+
+    @staticmethod
+    def _serialization_to_xml(song):
+        song_info = et.Element('song', attrib={'id': song.song_id})
+        title = et.SubElement(song_info, 'title')
+        title.text = song.title
+        artist = et.SubElement(song_info, 'artist')
+        artist.text = song.artist
+        return et.tostring(song_info, encoding='unicode')
+
+    def _get_serializer(self, _format):
         if _format == "json":
-            song_info = {
-                'id': song.id,
-                'title': song.title,
-                'artist': song.artist
-            }
-            return json.dumps(song_info)
+            return self._serialization_to_json
         elif _format == "xml":
-            song_info = et.Element('song', attrib={'id': song.song_id})
-            title = et.SubElement(song_info, 'title')
-            title.text = song.title
-            artist = et.SubElement(song_info, 'artist')
-            artist.text = song.artist
-            return et.tostring(song_info, encoding='unicode')
+            return self._serialization_to_xml
         else:
-            raise NotImplementedError(
-                "For now we only support serializing the json and xml type."
-            )
+            raise ValueError(_format)
+
+    def serializer(self, song, _format):
+        serializer = self._get_serializer(_format)
+        return serializer(song)
